791f4598e9b24ea84ea978b88663d8e77651e6c6
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2023-03-17 14:06:25 +0100

    Install and configure Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Head from "next/head";
 
 export default function Home() {
-  return <>
-    <Head>
-      <title>Maturzystnie</title>
-    </Head>
-    <h1>Maturzystnie</h1>
-  </>
+    return (
+        <>
+            <Head>
+                <title>Maturzystnie</title>
+            </Head>
+            <h1>Maturzystnie</h1>
+        </>
+    );
 }
