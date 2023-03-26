fe7ea58383daa4dc9d1b345859e127b716a8afad
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2023-03-26 22:16:05 +0200

    Add min-height in FeaturesBanner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
                 <WelcomeBanner />
                 <InvisibleSeparator />
                 <FeaturesBanner />
+                <InvisibleSeparator />
             </main>
         </>
     );
